4687c2cc01d496be4a6dba433ce062a5c0de7615
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-05-27 16:27:07 +0200

    This was the cause of not working at all

--- app/code/Test/Demo/registration.php
+++ app/code/Test/Demo/registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Module_Test', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Test_Demo', __DIR__);
