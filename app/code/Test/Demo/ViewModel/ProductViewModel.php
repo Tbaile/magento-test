9e98bfeb77fb1db972ae92770a7eca6df55f6106
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-06-07 17:12:07 +0200

    Edited ViewModel, added logic to demo.phtml
    Added custom CSS for instant-buy feature

--- app/code/Test/Demo/ViewModel/ProductViewModel.php
+++ app/code/Test/Demo/ViewModel/ProductViewModel.php
@@ -4,28 +4,44 @@ namespace Test\Demo\ViewModel;
 
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\ProductRepository;
+use Magento\Framework\Api\AttributeInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class ProductViewModel implements ArgumentInterface
 {
+    private ?bool $expressDelivery = null;
+    private ?string $expressDeliveryMessage = null;
+
     public function __construct(
         private readonly ProductRepository $productRepository,
         private readonly RequestInterface $request
     )
     {
-    }
-
-    public function getProduct(?string $id = null): ?Product
-    {
         try {
-            if (is_null($id)) {
-                return $this->productRepository->getById($this->request->getParam('id'));
-            }
-            return $this->productRepository->getById($id);
+            /** @var Product $product */
+            $product = $this->productRepository->getById($this->request->getParam('id'));
+            $this->expressDelivery = $product->getData('express_delivery');
+            $this->expressDeliveryMessage = $product->getData('express_delivery_message');
         } catch (NoSuchEntityException) {
-            return null;
         }
     }
+
+    /**
+     * @return bool|\Magento\Framework\Api\AttributeInterface|null
+     */
+    public function getExpressDelivery(): AttributeInterface|bool|null
+    {
+        return $this->expressDelivery;
+    }
+
+    /**
+     * @return \Magento\Framework\Api\AttributeInterface|string|null
+     */
+    public function getExpressDeliveryMessage(): AttributeInterface|string|null
+    {
+        return $this->expressDeliveryMessage;
+    }
+
 }
